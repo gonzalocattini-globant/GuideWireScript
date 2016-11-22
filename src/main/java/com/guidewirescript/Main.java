312ea92e3971c1e0b5cde00f5844c1d9c7bb7316
gonzalocattini-globant <gonzalo.cattini@globant.com>
2016-11-22 14:59:29 -0300

    start

--- src/main/java/com/guidewirescript/Main.java
+++ src/main/java/com/guidewirescript/Main.java
@@ -5,10 +5,37 @@
  */
 package com.guidewirescript;
 
+import java.util.Arrays;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 /**
  *
  * @author gonzalo.cattini
  */
 public class Main {
+    
+    public static void main(String[] args){
+        
+        System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+        
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability("chrome.switches", Arrays.asList("--ignore-certificate-errors"));
+        
+        WebDriver driver = new ChromeDriver(capabilities);
+        driver.get("https://localhost:8082/cc");
+        
+        WebElement username = driver.findElement(By.cssSelector("#Login:LoginScreen:LoginDV:username-inputEl"));
+        WebElement password = driver.findElement(By.cssSelector("#Login:LoginScreen:LoginDV:password-inputEl"));
+        
+        username.sendKeys("su");
+        password.sendKeys("gw");
+        
+        driver.findElement(By.id("Login:LoginScreen:LoginDV:submit")).click();
+        
+    }
     
 }
